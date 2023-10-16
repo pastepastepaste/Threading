42514f8c7883411a0470022f3fc8de90d8c7d7b2
Ann Villegas <144677995+pastepastepaste@users.noreply.github.com>
2023-10-16 19:13:54 +0800

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace Activity
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label = new System.Windows.Forms.Label();
+            this.btn = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Tai Le", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(79, 53);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(248, 26);
+            this.label.TabIndex = 0;
+            this.label.Text = "- Before Starting Thread -";
+            // 
+            // btn
+            // 
+            this.btn.BackColor = System.Drawing.Color.RosyBrown;
+            this.btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn.FlatStyle = System.Windows.Forms.FlatStyle.System;
+            this.btn.Font = new System.Drawing.Font("Microsoft Tai Le", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn.ForeColor = System.Drawing.Color.LightCoral;
+            this.btn.Location = new System.Drawing.Point(123, 115);
+            this.btn.Name = "btn";
+            this.btn.Size = new System.Drawing.Size(155, 61);
+            this.btn.TabIndex = 1;
+            this.btn.Text = "Run";
+            this.btn.UseVisualStyleBackColor = false;
+            this.btn.Click += new System.EventHandler(this.btn_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.IndianRed;
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(394, 28);
+            this.panel1.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.RosyBrown;
+            this.ClientSize = new System.Drawing.Size(394, 223);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.btn);
+            this.Controls.Add(this.label);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.Button btn;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(" - Before starting - ");
+            ThreadStart Thread = new ThreadStart(MyThreadClass.Thread1);
+            Thread ThreadA = new Thread(Thread);
+            ThreadA.Start();
+            ThreadA.Name = "Thread A";
+
+            Thread ThreadB = new Thread(Thread);
+            ThreadB.Start();
+            ThreadB.Name = "Thread B";
+            
+          
+            
+          
+            ThreadA.Join();
+            ThreadB.Join();
+
+            label.Text = "- End of Thread ";
+            Console.WriteLine(label.Text.ToString());
+        }
+    }
+}
